b9cb52ca237a7ec4e23eda1b0f1f6cfc09d808b4
chanbopich11tann <143944220+chanbopich11tann@users.noreply.github.com>
2023-12-11 04:01:06 -0500

    Update

--- final-project-api/example1.py
+++ final-project-api/example1.py
@@ -13,4 +13,31 @@ credential = json.load(open('credential.json'))
 API_KEY = credential['API_KEY']
 ENDPOINT = credential['ENDPOINT']
 
-cv_client = ComputerVisionClient(ENDPOINT, API_KEY)
+cv_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(API_KEY))
+
+image_url = ''
+local_file = './Image/1.jpg'
+response = cv_client.read(url =image_url,Language='en', raw=True)
+response = cv_client.read_in_stream(open(local_file, 'rb')
+)
+operationLocation = response.headers['Operation-Location']
+Operation_id = operationLocation.split('/')[-1]
+result = cv_client.get_read_result(operation_id)
+
+print(result)
+print(result.status)
+print(result.analyze_result)
+
+if result.status == OperationStatusCodes.succeeded:
+    read_results = result.analyze_result.read_results
+    for analyzed_result in read_results:
+        for line in analyzed_result.lines:
+            print('line: ')
+            print(line.text)
+            for word in line.words:
+                print('Words:')
+                print(word.text)
+            break
+
+{'additional_properties': {}, 'language': None,
+'bounding_box': []}
